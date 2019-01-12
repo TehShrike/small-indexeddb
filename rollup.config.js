53e2616d936cbae4a4109d2a93b3897465ea7c63
Josh Duff <me@JoshDuff.com>
2019-01-12 13:41:17 -0600

    Update rollup, drop tape in favor of zora

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,17 @@
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
-import builtins from 'rollup-plugin-node-builtins'
-import globals from 'rollup-plugin-node-globals'
 
 export default {
-	name: `smallIndexedDbTest`,
 	input: `./test.js`,
 	output: {
+		name: `smallIndexedDbTest`,
 		format: `iife`,
+		sourcemap: `inline`,
 	},
-	sourcemap: `inline`,
 	plugins: [
 		commonjs(),
 		resolve({
 			browser: true,
 		}),
-		builtins(),
-		globals(),
 	],
 }
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const test = require('tape-promise/tape.js')
-const smallIndexedDb = require('./index.js')
+const test = require(`zora`)
+const smallIndexedDb = require(`./index.js`)
 
-test(`Read empty key`, t => smallIndexedDb(`dummyDb`, 'dummyStore').then(store =>
+test(`Read empty key`, t => smallIndexedDb(`dummyDb`, `dummyStore`).then(store =>
 	store.read([ `wat` ]).then(results => {
 		t.ok(Array.isArray(results))
 		t.equal(results.length, 1)
@@ -9,80 +9,80 @@ test(`Read empty key`, t => smallIndexedDb(`dummyDb`, 'dummyStore').then(store =
 	}))
 )
 
-test('Write values, then read them back', async t => {
-	const store = await smallIndexedDb('someStore')
+test(`Write values, then read them back`, async t => {
+	const store = await smallIndexedDb(`someStore1`)
 
 	await store.write([
-		[ 'key1', 'value1' ],
-		{ key: 'key2', value: 'value2' },
+		[ `key1`, `value1` ],
+		{ key: `key2`, value: `value2` },
 	])
 
-	const [ value2 ] = await store.read([ 'key2' ])
+	const [ value2 ] = await store.read([ `key2` ])
 
-	t.equal(value2, 'value2')
+	t.equal(value2, `value2`)
 
-	const [ value1 ] = await store.read([ 'key1' ])
+	const [ value1 ] = await store.read([ `key1` ])
 
-	t.equal(value1, 'value1')
+	t.equal(value1, `value1`)
 })
 
-test('Write some values, delete one, then read them', async t => {
-	const store = await smallIndexedDb('someStore')
+test(`Write some values, delete one, then read them`, async t => {
+	const store = await smallIndexedDb(`someStore2`)
 
 	await store.write([
-		[ 'key1', 'value1' ],
-		[ 'key2', 'value2' ],
-		[ 'key3', 'value3' ],
+		[ `key1`, `value1` ],
+		[ `key2`, `value2` ],
+		[ `key3`, `value3` ],
 	])
 
-	await store.delete([ 'key2' ])
+	await store.delete([ `key2` ])
 
-	const [ value1, value2, value3 ] = await store.read([ 'key1', 'key2', 'key3' ])
+	const [ value1, value2, value3 ] = await store.read([ `key1`, `key2`, `key3` ])
 
-	t.equal(value1, 'value1')
+	t.equal(value1, `value1`)
 	t.equal(value2, undefined)
-	t.equal(value3, 'value3')
+	t.equal(value3, `value3`)
 })
 
 test(`Write some values, then clear 'em out`, async t => {
-	const store = await smallIndexedDb('someStore')
+	const store = await smallIndexedDb(`someStore3`)
 
 	await store.write([
-		[ 'key1', 'value1' ],
+		[ `key1`, `value1` ],
 	])
 
 	await store.clear()
 
-	const [ value1 ] = await store.read([ 'key1' ])
+	const [ value1 ] = await store.read([ `key1` ])
 
 	t.equal(value1, undefined)
 })
 
 test(`Write an object`, async t => {
-	const store = await smallIndexedDb('someStore')
+	const store = await smallIndexedDb(`someStore4`)
 
 	await store.write([
-		[ 'key1', { value1: true }],
+		[ `key1`, { value1: true }],
 	])
 
-	const [ object ] = await store.read([ 'key1' ])
+	const [ object ] = await store.read([ `key1` ])
 
 	t.deepEqual(object, { value1: true })
 })
 
 test(`Different dbs don't affect each other`, async t => {
-	const store1 = await smallIndexedDb('store1')
-	const store2 = await smallIndexedDb('store2')
+	const store1 = await smallIndexedDb(`store1`)
+	const store2 = await smallIndexedDb(`store2`)
 
 	await store1.write([
-		[ 'key1', 'valuez' ],
+		[ `key1`, `valuez` ],
 	])
 
 	await store2.write([
-		[ 'key1', 'VALUUUEEEEE' ],
+		[ `key1`, `VALUUUEEEEE` ],
 	])
 
-	const [ value ] = await store1.read([ 'key1' ])
+	const [ value ] = await store1.read([ `key1` ])
 
-	t.equal(value, 'valuez')
+	t.equal(value, `valuez`)
 })
